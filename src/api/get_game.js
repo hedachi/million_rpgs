2e4f2eec3e8d12d07e754b29058ebb60c3ae16a3
Takayuki Fukuda <fukuda_82@yahoo.co.jp>
2024-07-08 11:32:55 +0900

    プロンプト調整、デバッグコード

--- src/api/get_game.js
+++ src/api/get_game.js
@@ -17,6 +17,9 @@ module.exports.handler = async (event) => {
 
     // データを取得
     const data = await dynamodb.get(params).promise();
+    // if (data?.Item?.stories.length > 0) {
+    //   data.Item.stories[0] = "[damage:player,mid]\n[damage:partner,high]\n[damage:101,low]\n" + data.Item.stories[0];
+    // }
     return {
       statusCode: 200,
       body: JSON.stringify({
