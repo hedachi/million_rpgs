0dfd75be3e5f7c4d250f370b192e7a9afa7e2e6f
Takayuki Fukuda <fukuda_82@yahoo.co.jp>
2024-07-17 17:47:45 +0900

    CORS対応

--- src/api/get_game.js
+++ src/api/get_game.js
@@ -22,6 +22,11 @@ module.exports.handler = async (event) => {
     // }
     return {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": '*',
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+      },
       body: JSON.stringify({
         gameDetails: data.Item,
       }),
--- src/api/play_game.js
+++ src/api/play_game.js
@@ -45,6 +45,11 @@ module.exports.handler = async (event) => {
 
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Origin": '*',
+      "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+    },
     body: JSON.stringify({
       gameDetail: gameDetail,
     }),
